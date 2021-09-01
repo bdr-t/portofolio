be08f8d4ffb9e8626f655a4ccb0dbe72cb8d3baf
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-09-01 18:28:04 +0200

    add technologies component

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,84 @@
+import {
+  Container,
+  Grid,
+  Flex,
+  IconContainer,
+  ReactI,
+} from "./Technologies.styles";
+import { FaReact, FaGithub } from "react-icons/fa";
+import {SiMongodb, SiPostman, SiCss3, SiSocketDotIo, SiRabbitmq, SiRedux, SiTailwindcss, SiTypescript, SiVisualstudiocode, SiJavascript, SiHtml5, SiNodeDotJs} from "react-icons/si"
+import {BsBootstrapFill} from "react-icons/bs"
+import express from '../../images/express.png'
+import figma from '../../images/figma.png'
+import python from '../../images/python.png'
+import postgres from '../../images/postgres.png'
+
+
+const Technologies = () => {
+  return (
+    <Container>
+      <h2>Technologies</h2>
+      <Flex>
+        <IconContainer>
+          <FaReact color="#61DAFB" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiRedux color="#764ABC" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <BsBootstrapFill color="#7952B3" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiTailwindcss color="#06B6D4" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <FaGithub color="#24292F" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiTypescript color="#3178C6" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiVisualstudiocode color="#23A7F2" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiJavascript color="#FCDC00" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiHtml5 color="#FF8129" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiNodeDotJs color="#78B460" size="50" />
+        </IconContainer>
+        <IconContainer>
+            <img src={express} alt="" />
+        </IconContainer>
+        <IconContainer>
+          <SiRabbitmq color="#FF6600" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiSocketDotIo color="black" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiCss3 color="#3595CF" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiPostman color="#FF6C37" size="50" />
+        </IconContainer>
+        <IconContainer>
+          <SiMongodb color="#13AA52" size="50" />
+        </IconContainer>
+        <IconContainer>
+            <img src={postgres} alt="" />
+        </IconContainer>
+        <IconContainer>
+            <img src={python} alt="" />
+        </IconContainer>
+        <IconContainer>
+            <img src={figma} alt="" />
+        </IconContainer>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Technologies;
---
+++ src/components/Technologies/Technologies.styles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+  width: 75%;
+
+  /* background-color: lightgreen; */
+  margin: 50px auto;
+  padding: 1em;
+
+  h2 {
+    font-size: 50px;
+    font-weight: 700;
+    background: linear-gradient(to right, #680641, #bc0341, #ea0140);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    height: 60px;
+  }
+
+  p {
+    font-size: 18px;
+  }
+
+  img{
+    width: 50px;
+    height: auto;
+  }
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  padding: 1em;
+  grid-template-columns: 1fr 1fr 1fr;
+  height: 100%;
+  width: 100%;
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  margin-top: 2em;
+  gap: 1em;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const IconContainer = styled.div`
+  width: 75px;
+  height: 75px;
+  /* background-color: black; */
+  border-radius: 4px;
+  display: grid;
+  place-content: center;
+  border: 2px solid #680641;
+  border: 2px solid transparent;
+  background-image: linear-gradient(to right,#680641 ,#ea0140),
+    linear-gradient(to bottom, #ea0140, #680641),
+    linear-gradient(to left, #680641, #ea0140),
+    linear-gradient(to top, #ea0140, #680641);
+  background-origin: border-box;
+  background-size: 100% 3px, 3px 100%, 100% 3px, 3px 100%;
+  background-position: top left, top right, bottom right, bottom left;
+  background-repeat: no-repeat;
+`;
+
