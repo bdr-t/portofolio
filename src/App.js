59a7aefad374d71bb4e9e4835d3dcc11cca7d881
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-09-13 16:44:08 +0200

    add : wakeupheroku

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ import Technologies from "./components/Technologies/Technologies";
 import AboutMe from "./components/AboutMe/AboutMe";
 
 import Footer from "./components/Footer/Footer";
+import {useEffect} from "react"
+
+
 
 function App() {
+
+  useEffect(()=>{
+    function wakeUpHeroku(){
+      fetch('https://lyrics-spotify-back.herokuapp.com/lyrics')
+      fetch('https://memories-bader.herokuapp.com')
+      fetch('https://proxy-bt.herokuapp.com/')
+    }
+    wakeUpHeroku()
+    
+  }, [])
   return (
     <div className="App">
       <Nav />
